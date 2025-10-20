490f0260ae18ce6fb838a314d646b33afe4b72bb
Dilyana Zheleva <di.zheleva89@gmail.com>
2025-10-21 02:30:02 +0300

    add AutoMapper profile for Client <-> DTO

---
+++ ClientHub.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace ClientHub.Api.Mapping
+{
+    using AutoMapper;
+    using ClientHub.Api.Dtos;
+    using ClientHub.Api.Models;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client, ClientDto>();
+            CreateMap<CreateClientDto, Client>();
+            CreateMap<UpdateClientDto, Client>();
+        }
+    }
+}
